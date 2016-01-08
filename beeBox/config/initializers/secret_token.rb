8241d5a7da91878c908ba1ba5f112bee379613b6
d4rk-m4tt3r <bccphysicsclub@gmail.com>
2016-01-07 23:03:14 -0500

    all

---
+++ beeBox/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeeBox::Application.config.secret_key_base = '785551431bf6b1aba96a76cbda37ba51b70a8ecd0fb7040649e534c1ebb62fd198235e6a88b55521b89af0c34e057c1ff47f5ed237db7ad4728b1f643e7ff29a'
